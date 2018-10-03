44d87d77623a459635b2de8ddfdd0153d777990f
Gillis Bernard <gillis.c.bernard@gmail.com>
2018-10-03 09:38:36 -0400

    setting up flexbox

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -3,3 +3,13 @@ function addNewList() {
     console.log('hello world console');
 }
 
+$(document).ready(function(){
+    $("#add-item").click(function() { // bind handler for click event
+        console.log("hello")
+        var list = $("#grocery-list"); // get the ol list by id
+        var itemInput = $("#new-list-item"); // get the new item input
+	  // append the input value within an li element
+        list.append("<li>" + itemInput.val() + "</li>"); 
+    });
+});
+
