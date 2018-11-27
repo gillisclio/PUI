db2deb36e603ab714cbed58962b48b466d780c49
Gillis Bernard <gillis.c.bernard@gmail.com>
2018-11-27 14:02:04 -0500

    Big progress!
    setting up js for scrolling, adding up to you can help block, debugged scroll to explore + squiggle

--- homework_7/script.js
+++ homework_7/script.js
@@ -1,6 +1,3 @@
-// // Run when document is ready, which means it is fully loaded into browser
-// $(function(){
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -17,4 +14,33 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-// })
+
+// // Run when document is ready, which means it is fully loaded into browser
+$(function(){
+
+
+
+//
+$(window).on('scroll', function(){
+	var heartTop = $('.heart').offset().top,
+   	heartHeight = $('.heart').outerHeight(),
+   	windowHeight = $(window).height(),
+   	windowScroll = $(this).scrollTop();
+
+   	if (windowScroll > (heartTop+heartHeight-windowHeight/2)) {
+   		$(".heart").addClass("fadeInUp");
+   	}
+})
+
+$(window).on('scroll', function(){
+	var rayTop = $('.ray').offset().top,
+   	rayHeight = $('.ray').outerHeight(),
+   	windowHeight = $(window).height(),
+   	windowScroll = $(this).scrollTop();
+
+   	if (windowScroll > (rayTop+rayHeight-windowHeight/2)) {
+   		$(".ray").addClass("fadeInUp");
+   	}
+})
+
+})
